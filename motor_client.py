a57075deaf45ec5a027b500adafd61c47c895a6e
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-06-08 15:42:38 -0400

    separate

---
+++ motor_client.py
@@ -0,0 +1,26 @@
+from AX12 import Ax12
+import socketio
+sio = socketio.Client()
+
+
+# e.g 'COM3' windows or '/dev/ttyUSB0' for Linux
+Ax12.DEVICENAME = '/dev/ttyUSB0'
+
+Ax12.BAUDRATE = 1000000
+
+# sets baudrate and opens com port
+Ax12.connect()
+
+# create AX12 instance with motors ID
+motor1 = 1
+motor2 = 2
+my_dxl1 = Ax12(motor1)
+my_dxl2 = Ax12(motor2)
+
+my_dxl1.set_moving_speed(500)
+my_dxl2.set_moving_speed(500)
+
+angle1 = 0
+angle2 = 0
+
+my_dxl1.set_torque_enable(True)
