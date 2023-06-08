a6fd36472da001cc5b9951667e13480c5a7e4bbf
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-06-08 15:46:35 -0400

    Update motor_client.py

--- motor_client.py
+++ motor_client.py
@@ -17,10 +17,12 @@ motor2 = 2
 my_dxl1 = Ax12(motor1)
 my_dxl2 = Ax12(motor2)
 
-my_dxl1.set_moving_speed(500)
+my_dxl1.set_torque_enable(True)
 my_dxl2.set_moving_speed(500)
 
 angle1 = 0
 angle2 = 0
 
-my_dxl1.set_torque_enable(True)
+while True:
+    input_pos = int(input("goal pos: "))
+    my_dxl1.set_moving_speed(input_pos)
