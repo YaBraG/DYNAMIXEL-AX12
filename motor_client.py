e674086d30c920a2890a9fe1393330689e0b9c1a
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-06-09 11:17:17 -0400

    Adjust

--- motor_client.py
+++ motor_client.py
@@ -1,30 +1,27 @@
 from AX12 import Ax12
 import socketio
+
 sio = socketio.Client()
 
-
-# e.g 'COM3' windows or '/dev/ttyUSB0' for Linux
+# e.g 'COM11' windows or '/dev/ttyUSB0' for Linux/Raspberry
 Ax12.DEVICENAME = '/dev/ttyUSB0'
-
 Ax12.BAUDRATE = 1000000
 
 # sets baudrate and opens com port
 Ax12.connect()
 
 # create AX12 instance with motors ID
-motor1 = 1
-motor2 = 2
-my_dxl1 = Ax12(motor1)
-my_dxl2 = Ax12(motor2)
+motor1 = Ax12(1)
+motor2 = Ax12(2)
 
-my_dxl1.enable_torque()
-my_dxl2.set_moving_speed(500)
-my_dxl1.set_torque_limit(1023)
+motor1.enable_torque()
+motor1.set_moving_speed(1023)
+motor2.set_moving_speed(500)
+motor1.set_torque_limit(1023)
 
 angle1 = 0
 angle2 = 0
 
 while True:
     input_pos = int(input("goal pos: "))
-    my_dxl1.set_moving_speed(input_pos)
-    my_dxl1.set_goal_position(input_pos)
+    motor1.set_goal_position(input_pos)
--- servo_client.py
+++ servo_client.py
@@ -1,40 +1,27 @@
 from AX12 import Ax12
 import socketio
-sio = socketio.Client()
 
-
-# e.g 'COM3' windows or '/dev/ttyUSB0' for Linux
+# 'COM11' windows or '/dev/ttyUSB0' for Linux/Raspberry
 Ax12.DEVICENAME = '/dev/ttyUSB0'
-
 Ax12.BAUDRATE = 1000000
 
 # sets baudrate and opens com port
 Ax12.connect()
 
 # create AX12 instance with motors ID
-motor1 = 1
-motor2 = 2
-my_dxl1 = Ax12(motor1)
-my_dxl2 = Ax12(motor2)
+motor1 = Ax12(1)
+motor2 = Ax12(2)
 
-my_dxl1.set_moving_speed(1023)
-my_dxl2.set_moving_speed(1023)
+motor1.set_moving_speed(1023)
+motor2.set_moving_speed(1023)
 
 angle1 = 0
 angle2 = 0
 
-
-# Enable if manual input wanted
-# def user_input():
-#     """Check to see if user wants to continue"""
-#     ans = input('Continue? : y/n ')
-#     if ans == 'n':
-#         return False
-#     else:
-#         return True
-
-
 # Connect to server to recieve/emmit data
+sio = socketio.Client()
+
+
 @sio.event
 def connect():
     print('connection established')
@@ -51,12 +38,6 @@ def my_message(data):
 def disconnect():
     print('disconnected from server')
 
-# @sio.on('order')
-# def on_message(data):
-#     print(data)
-#     angle = (int(data))
-#     my_dxl1.set_goal_position(angle)
-
 
 @sio.on('h-order')
 def on_message(h):
@@ -64,40 +45,51 @@ def on_message(h):
     header = (int(h))
     if (header > 1023):
         header = 1023
-    my_dxl1.set_goal_position(header)
+    motor1.set_goal_position(header)
 
 
 @sio.on('mouse-order')
 def on_message(pitch, yaw):
     print(pitch, yaw)
-    # angle1 = (int(pitch))
+    angle1 = (int(pitch))
     angle2 = (int(yaw))
-    # my_dxl1.set_goal_position(angle1)
-    my_dxl2.set_goal_position(angle2)
+    motor1.set_goal_position(angle1)
+    motor2.set_goal_position(angle2)
+
+
+"""
+# Enable if manual input wanted
+# def user_input():
+    # Check to see if user wants to continue
+#     ans = input('Continue? : y/n ')
+#     if ans == 'n':
+#         return False
+#     else:
+#         return True
 
 # Starts motor (MANUAL INPUT ONLY)
-    # def main(motor_object):
-    #     """ sets goal position based on user input """
-    #     bool_test = True
-    #     while bool_test:
-    #         print("\nPosition of dxl ID: %d is %d " %
-    #               (motor_object.id, motor_object.get_present_position()))
-    #         # desired angle input
-    #         input_pos = angle
-    #         motor_object.set_goal_position(input_pos)
-    #         print("Position of dxl ID: %d is now: %d " %
-    #               (motor_object.id, motor_object.get_present_position()))
+# def main(motor_object):
+#     sets goal position based on user input
+#     bool_test = True
+#     while bool_test:
+#         print("\nPosition of dxl ID: %d is %d " %
+#               (motor_object.id, motor_object.get_present_position()))
+#         # desired angle input
+#         input_pos = angle
+#         motor_object.set_goal_position(input_pos)
+#         print("Position of dxl ID: %d is now: %d " %
+#               (motor_object.id, motor_object.get_present_position()))
 
-    # return motor_object.id
-    #   bool_test = user_input()
+# return motor_object.id
+#   bool_test = user_input()
 
-    # pass in AX12 object
-    # main(my_dxl)
+# pass in AX12 object
+# main(my_dxl)
 
-    # disconnect
-    # my_dxl.set_torque_enable(0)
-    # Ax12.disconnect()
-
+# disconnect
+# my_dxl.set_torque_enable(0)
+# Ax12.disconnect()
+"""
 
 sio.connect('http://192.168.2.11:3000')
 sio.wait()
