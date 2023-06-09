74095c1c5be1b8a9a86294cabf77c18c341106f6
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-06-09 16:55:32 -0400

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,102 @@
-int
+from AX12 import Ax12
+import math
+import socketio
+import time
+
+
+def remap(x, oMin, oMax, nMin, nMax):
+
+    # range check
+    if oMin == oMax:
+        print("Warning: Zero input range")
+        return None
+
+    if nMin == nMax:
+        print("Warning: Zero output range")
+        return None
+
+    # check reversed input range
+    reverseInput = False
+    oldMin = min(oMin, oMax)
+    oldMax = max(oMin, oMax)
+    if not oldMin == oMin:
+        reverseInput = True
+
+    # check reversed output range
+    reverseOutput = False
+    newMin = min(nMin, nMax)
+    newMax = max(nMin, nMax)
+    if not newMin == nMin:
+        reverseOutput = True
+
+    portion = (x-oldMin)*(newMax-newMin)/(oldMax-oldMin)
+    if reverseInput:
+        portion = (oldMax-x)*(newMax-newMin)/(oldMax-oldMin)
+
+    result = portion + newMin
+    if reverseOutput:
+        result = newMax - portion
+
+    return result
+
+
+sio = socketio.Client()
+
+# 'COM11' windows or '/dev/ttyUSB0' for Linux/Raspberry
+Ax12.DEVICENAME = '/dev/ttyUSB0'
+Ax12.BAUDRATE = 1000000
+
+# sets baudrate and opens com port
+Ax12.connect()
+
+# create AX12 instance with motors ID
+motor1 = Ax12(1)
+motor2 = Ax12(2)
+
+motor1.enable_torque()
+motor2.enable_torque()
+motor1.set_moving_speed(500)
+motor2.set_moving_speed(500)
+motor1.set_torque_limit(1023)
+motor2.set_torque_limit(1023)
+
+
+@sio.event
+def connect():
+    print('connection established')
+    sio.emit("ID", 'python-servo-client')
+
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+
+@sio.on('drive-orders')
+def on_message(angle, speed):
+    # newSpeed = 0
+    # newAngle = 0
+    if speed < 0:
+        newSpeed = round(remap(speed, -1, 0, 1024, 2047))
+
+    if speed > 0:
+        newSpeed = round(remap(speed, 0, 1, 0, 1023))
+
+    if angle < 0:
+        newAngle = round(remap(angle, -1, 0, 1024, 2047))
+
+    if angle > 0:
+        newAngle = round(remap(angle, 0, 1, 0, 1023))
+
+    motor1.set_moving_speed(newAngle)
+    motor2.set_moving_speed(newSpeed)
+
+
+sio.connect('http://192.168.2.11:3000')
+sio.wait()
