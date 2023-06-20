649f2e5f3e9ce0122e1ef7338bd35cfffcba53b4
YaBraG <92254077+YaBraG@users.noreply.github.com>
2023-06-20 11:59:03 -0400

    Update classh.py

--- classh.py
+++ classh.py
@@ -1,5 +1,7 @@
 import socketio
 import math
+import keyboard
+import time
 
 
 def remap(x, oMin, oMax, nMin, nMax):
@@ -57,37 +59,41 @@ def my_message(data):
 def disconnect():
     print('disconnected from server')
 
-n = 0
+
 @sio.on('drive-orders')
 def on_message(angle, speed):
-   
-    while (n < 10):
-        asMultiplier = angle * speed
-        # # newAngle = round(math.pow(asMultiplier, 3), 4)
-        # newAngle = round(remap(asMultiplier, 0, 180, 0, 1023))
-        if speed < 0.05:
-            motor1Speed = 0
-            motor2Speed = 0
-        if angle == 90:
-            motor1Speed = 1023
-            motor2Speed = 2047
-        elif angle == -90:
-            motor1Speed = 2047
-            motor2Speed = 1023
-        elif angle >= 0 and angle < 90:
-            motor1Speed = 1023 * speed
-            motor2Speed = round(remap(asMultiplier, 0, 90, 0, 2047))
-        elif angle > 90 and angle <= 180:
-            motor1Speed = round(remap(asMultiplier, 90, 180, 0, 1023))
-            motor2Speed = 2047 * speed
-        elif angle < 0 and angle > -90:
-            motor1Speed = round(remap(asMultiplier, -90, 0, 2047, 0))
-            motor2Speed = 1023 * speed
-        elif angle < -90 and angle >= -180:
-            motor1Speed = 2047 * speed
-            motor2Speed = round(remap(asMultiplier, -180, -90, 0, 1023))
-        n += 1
-        print(motor1Speed + " | " + motor2Speed)
+    asMultiplier = angle * speed
+
+    # # newAngle = round(math.pow(asMultiplier, 3), 4)
+    # newAngle = round(remap(asMultiplier, 0, 180, 0, 1023))
+    if angle == 90:
+        motor1Speed = 1023
+        motor2Speed = 2047
+
+    elif angle == -90:
+        motor1Speed = 2047
+        motor2Speed = 1023
+
+    elif angle >= 0 and angle < 90:
+        motor1Speed = round(1023 * speed)
+        motor2Speed = round(remap(asMultiplier, 0, 90, 0, 2047))
+
+    elif angle > 90 and angle <= 180:
+        motor1Speed = round(remap(asMultiplier, 90, 180, 0, 1023))
+        motor2Speed = round(2047 * speed)
+
+    elif angle < 0 and angle > -90:
+        motor1Speed = round(remap(asMultiplier, -90, 0, 2047, 0))
+        motor2Speed = round(1023 * speed)
+
+    elif angle < -90 and angle >= -180:
+        motor1Speed = round(2047 * speed)
+        motor2Speed = round(remap(asMultiplier, -180, -90, 0, 1023))
+
+    if speed < 0.05:
+        motor1Speed = 0
+        motor2Speed = 0
+    print(f"M1 = {motor1Speed} | M2 = {motor2Speed}")
 
 
 sio.connect('http://192.168.2.11:3000')
